a9c8b94d20dbf382089dac4260e47d8776f20558
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2018-01-28 22:02:42 -0200

    update cap 4 escrito

--- compile.sh
+++ compile.sh
@@ -18,7 +18,7 @@ echo "#################################"
 echo "#################################"
 echo "#################################"
 pdflatex -synctex=1 -interaction=nonstopmode --src-specials main
-#for file in main.aux main.bbl main.blg main.log main.lof main.out main.toc
-#do
-#  rm -f $file
-#done
+for file in main.aux main.blg main.log main.lof main.out main.toc
+do
+  rm -f $file
+done
